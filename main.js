004f27d4168bcf8a590c2d5f0b5c1d055a0ee8a0
Ichikawa Tomoya <ichikawatomoya0705@gmail.com>
2024-10-19 23:49:11 +0900

    practice_complete

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
-const val = {
-  name: "ahiru",
-  age: 31,
-}
-
-const text=`my name is ${val.name}`;
-console.log(text);
-
-
-// const func1 = function(str) {
-//   return str;
+// const val = {
+//   name: "ahiru",
+//   age: 31,
 // }
 
-// console.log(func1("hello!"));
+// const text=`my name is ${val.name}`;
+// console.log(text);
 
-const func2 = str => str;
 
-console.log(func2("good!"));
+// // const func1 = function(str) {
+// //   return str;
+// // }
+
+// // console.log(func1("hello!"));
+
+// const func2 = str => str;
+
+// console.log(func2("good!"));
+
+
+// const sayHello = (name) => console.log(`こんにちは！${name}さん`);
+
+// sayHello("ahiru");
+
+// const name ="あ";
+// const age = 30;
+
+// const myProfile = {
+//   name,
+//   age,
+// }
+
+// console.log(myProfile);
+
+// const arr1 = [1,2];
+// console.log(...arr1);
+
+
+
+const nameArr = ["ahiru", "bhiru", "chiru"];
+
+nameArr.map((name) => {
+  console.log(name);
+});
